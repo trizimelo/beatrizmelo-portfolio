5b583456f60f62b193be14b73f1685815b43e6a4
trizimelo <beatrizdantas2312@gmail.com>
2026-07-23 15:14:31 -0300

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import ProfileHeader from "@/components/ProfileHeader";
 import TagFilter from "@/components/TagFilter";
 import ProjectCard from "@/components/ProjectCard";
 
+export const dynamic = "force-dynamic";
+
 interface SearchParams {
   tag?: string;
 }
