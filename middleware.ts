5592cb0c2f1a5d58dba413a6fa4af17f8d8ae653
trizimelo <beatrizdantas2312@gmail.com>
2026-07-25 13:13:56 -0300

    desativa temporariamente middleware do supabase

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,9 @@
 import { NextResponse, type NextRequest } from 'next/server'
-import { createClient } from './supabase/middleware'
 
 export function middleware(request: NextRequest) {
-  return createClient(request)
+  return NextResponse.next()
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'],
+  matcher: [],
 }
